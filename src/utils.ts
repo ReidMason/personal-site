e9d2033ca766063d1724d0a5f5584a0c23994538
Reid Mason <maddogshain132@gmail.com>
2023-01-30 21:21:10 +0000

    Added basic styling for blog pages

--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,14 @@ import { ROUTES } from "./consts";
 export const createBlogUrl = (slug: string) => {
   return `${ROUTES.blog}/${slug}`;
 };
+
+export const formatDate = (date: Date) => {
+  var options: Intl.DateTimeFormatOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  return date.toLocaleDateString("en-GB", options);
+};
